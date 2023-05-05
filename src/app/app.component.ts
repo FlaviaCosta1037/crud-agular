bbe69213a0a58a710a1ccae503b3f851e1a2a9df
FlaviaCosta1037 <88057693+FlaviaCosta1037@users.noreply.github.com>
2023-05-05 00:32:46 -0300

    Configuração de data no frontEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,171 +9,220 @@ import { Person } from './person';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-    styles: [`
+  styles: [
+    `
         :host ::ng-deep .p-dialog .product-image {
             width: 150px;
             margin: 0 auto 2rem auto;
             display: block;
         }
-    `],
-    styleUrls: ['./app.component.scss']
+    `,
+  ],
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { 
-    productDialog: boolean;
+export class AppComponent {
+  productDialog: boolean;
 
-    products: Product[];
+  products: Product[];
 
-    product: Product;
+  product: Product;
 
-    selectedProducts: Product[];
+  selectedProducts: Product[];
 
-    submitted: boolean;
+  submitted: boolean;
 
-    persons: Person[];
-    person: Person;
-    personDialog: boolean;
-    selectedPersons: Person[];
+  persons: Person[];
+  person: Person;
+  personDialog: boolean;
+  selectedPersons: Person[];
 
-    constructor(private productService: ProductService, private messageService: MessageService, private confirmationService: ConfirmationService, private personService: PersonService) { }
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private personService: PersonService
+  ) {}
 
-    ngOnInit() {
-        this.productService.getProducts().then(data => this.products = data);
-        this.personService.getPerson().then(data => this.persons = data);
-    }
+  ngOnInit() {
+    this.productService.getProducts().then((data) => (this.products = data));
+    this.personService.getPerson().then((data) => (this.persons = data));
+  }
 
-    openNew() {
+  openNew() {
+    this.person = {};
+    this.submitted = false;
+    this.productDialog = true;
+    this.personDialog = true;
+  }
+
+  deleteSelectedProducts() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete the selected products?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.products = this.products.filter(
+          (val) => !this.selectedProducts.includes(val)
+        );
+        this.selectedProducts = null;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Products Deleted',
+          life: 3000,
+        });
+      },
+    });
+  }
+
+  deleteSelectedPerson() {
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja deletar este usuário?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.persons = this.persons.filter(
+          (val) => !this.selectedPersons.includes(val)
+        );
+        this.selectedPersons = null;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Usuário Deletado',
+          life: 3000,
+        });
+      },
+    });
+  }
+
+  editProduct(product: Product) {
+    this.product = { ...product };
+    this.productDialog = true;
+  }
+
+  editPerson(person: Person) {
+    this.person = { ...person };
+    this.personDialog = true;
+  }
+
+  deleteProduct(product: Product) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + product.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.products = this.products.filter((val) => val.id !== product.id);
+        this.product = {};
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Product Deleted',
+          life: 3000,
+        });
+      },
+    });
+  }
+
+  deletePerson(person: Person) {
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja deletar? ' + person.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.persons = this.persons.filter((val) => val.id !== person.id);
         this.person = {};
-        this.submitted = false;
-        this.productDialog = true;
-        this.personDialog = true;
-    }
-
-    deleteSelectedProducts() {
-        this.confirmationService.confirm({
-            message: 'Are you sure you want to delete the selected products?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-                this.selectedProducts = null;
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Products Deleted', life: 3000});
-            }
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Usuário Deletado',
+          life: 3000,
         });
-    }
+      },
+    });
+  }
 
-    deleteSelectedPerson() {
-        this.confirmationService.confirm({
-            message: 'Tem certeza que deseja deletar este usuário?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.persons = this.persons.filter(val => !this.selectedPersons.includes(val));
-                this.selectedPersons = null;
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Usuário Deletado', life: 3000});
-            }
+  hideDialog() {
+    this.productDialog = false;
+    this.submitted = false;
+  }
+
+  saveProduct() {
+    this.submitted = true;
+
+    if (this.person.name.trim()) {
+      if (this.person.id) {
+        this.persons[this.findIndexById(this.person.id)] = this.person;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Usuário atualizado',
+          life: 3000,
         });
-    }
-
-    editProduct(product: Product) {
-        this.product = {...product};
-        this.productDialog = true;
-    }
-
-    editPerson(person: Person) {
-        this.person = {...person};
-        this.personDialog = true;
-    }
-    
-    deleteProduct(product: Product) {
-        this.confirmationService.confirm({
-            message: 'Are you sure you want to delete ' + product.name + '?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.products = this.products.filter(val => val.id !== product.id);
-                this.product = {};
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
-            }
+      } else {
+        this.person.id = this.createId();
+        this.persons.push(this.person);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Product Created',
+          life: 3000,
         });
-    }
+      }
 
-    deletePerson(person: Person) {
-        this.confirmationService.confirm({
-            message: 'Tem certeza que deseja deletar? ' + person.name + '?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.persons = this.persons.filter(val => val.id !== person.id);
-                this.person = {};
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Usuário Deletado', life: 3000});
-            }
+      this.persons = [...this.persons];
+      this.personDialog = false;
+      this.person = {};
+    }
+  }
+
+  savePerson() {
+    this.submitted = true;
+
+    if (this.person.name.trim()) {
+      if (this.person.id) {
+        this.persons[this.findIndexById(this.person.id)] = this.person;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Usuário atualizado',
+          life: 3000,
         });
+      } else {
+        this.person.id = this.createId();
+        this.persons.push(this.person);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Usuário Criado',
+          life: 3000,
+        });
+      }
+
+      this.persons = [...this.persons];
+      this.personDialog = false;
+      this.person = {};
+    }
+  }
+
+  findIndexById(id: string): number {
+    let index = -1;
+    for (let i = 0; i < this.persons.length; i++) {
+      if (this.persons[i].id === id) {
+        index = i;
+        break;
+      }
     }
 
+    return index;
+  }
 
-    hideDialog() {
-        this.productDialog = false;
-        this.submitted = false;
+  createId(): string {
+    let id = '';
+    var chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 5; i++) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
     }
-    
-    saveProduct() {
-        this.submitted = true;
-
-        if (this.person.name.trim()) {
-            if (this.person.id) {
-                this.persons[this.findIndexById(this.person.id)] = this.person;                
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Usuário atualizado', life: 3000});
-            }
-            else {
-                this.person.id = this.createId();
-                this.persons.push(this.person);
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
-            }
-
-            this.persons = [...this.persons];
-            this.personDialog = false;
-            this.person = {};
-        }
-    }
-
-    savePerson() {
-        this.submitted = true;
-
-        if (this.person.name.trim()) {
-            if (this.person.id) {
-                this.person[this.findIndexById(this.person.id)] = this.person;                
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Usuário atualizado', life: 3000});
-            }
-            else {
-                this.person.id = this.createId();
-                this.persons.push(this.person);
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Usuário Criado', life: 3000});
-            }
-
-            this.persons = [...this.products];
-            this.personDialog = false;
-            this.person = {};
-        }
-    }
-
-    findIndexById(id: string): number {
-        let index = -1;
-        for (let i = 0; i < this.persons.length; i++) {
-            if (this.persons[i].id === id) {
-                index = i;
-                break;
-            }
-        }
-
-        return index;
-    }
-
-    createId(): string {
-        let id = '';
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for ( var i = 0; i < 5; i++ ) {
-            id += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return id;
-    }
+    return id;
+  }
 }
